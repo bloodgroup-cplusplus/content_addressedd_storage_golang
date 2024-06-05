3f9f056188b4e92bd9a7d0d53b7450627db471fa
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2024-06-05 09:58:53 +0530

    adding mutexes for read

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -1,6 +1,16 @@
 package p2p
 
 // why
+import (
+	"net"
+	"sync"
+)
 
 type TCPTransport struct {
+	//
+	listenAddress string
+	listener      net.Listener
+
+	mu    sync.RWMutex
+	peers map[net.Addr]Peer
 }
